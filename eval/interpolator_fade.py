f1e587068047ecade2bea648573b669552bce3b8
jford49 <jford49@users.noreply.github.com>
2023-11-03 17:10:44 -0400

    Update interpolator_fade.py

--- eval/interpolator_fade.py
+++ eval/interpolator_fade.py
@@ -15,6 +15,7 @@ from . import util
 from absl import app
 from absl import flags
 import numpy as np
+import mediapy as media
 
 # Controls TF_CCP log level.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
@@ -62,6 +63,10 @@ _BLOCK_WIDTH = flags.DEFINE_integer(
     default=1,
     help='An int >= 1, number of patches along width, '
     'patch_width = width//block_width, should be evenly divisible.')
+_OUTPUT_VIDEO = flags.DEFINE_boolean(
+    name='output_video',
+    default=False,
+    help='If true, creates a video of the frames in the folder_out')
 
 def _run_interpolator() -> None:
   """Writes interpolated mid frames from a given folder."""
@@ -76,6 +81,9 @@ def _run_interpolator() -> None:
   
   if not path.isdir(_FOLDER_OUT.value):
     os.mkdir(_FOLDER_OUT.value)
+  if _OUTPUT_VIDEO.value:
+    ffmpeg_path = util.get_ffmpeg_path()
+    media.set_ffmpeg(ffmpeg_path)
 
   image_path_list1 = [f for f in listdir(_FOLDER_IN1.value) if path.isfile(path.join(_FOLDER_IN1.value, f))]
   image_path_list1.sort()
@@ -87,6 +95,7 @@ def _run_interpolator() -> None:
 
   mult = float(_FADE_COUNT.value)/float(n_files)
 
+  frames = List()
   idx = 0
   while idx < len(image_path_list):
     photo1_path = path.join(_FOLDER_IN.value, image_path_list1[idx])
@@ -120,6 +129,12 @@ def _run_interpolator() -> None:
     util.write_image(mid_frame_filepath, mid_frame)
     idx+=1
     img_idx+=1
+    if _OUTPUT_VIDEO.value:
+      frames.append(mid_frame)
+   
+  if _OUTPUT_VIDEO.value:
+     media.write_video(f'{_FOLDER_OUT.value}/interpolated.mp4', frames, fps=30)
+     logging.info('Output video saved at %s/interpolated.mp4.', _FOLDER_OUT.value)
   
 
 def main(argv: Sequence[str]) -> None:
