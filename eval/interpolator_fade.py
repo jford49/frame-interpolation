c2440b1d12bdd5b93a12b726e1fdac4611bce1c0
jford49 <jford49@users.noreply.github.com>
2023-11-03 18:08:02 -0400

    Update interpolator_fade.py

--- eval/interpolator_fade.py
+++ eval/interpolator_fade.py
@@ -112,12 +112,13 @@ def _run_interpolator() -> None:
     bot_idx = 0
     target_fade_idx = int(mult*float(idx))
 
+    current_fade_idx = 0
     while(True):
       # Invoke the model for one mid-frame interpolation.
       mid_frame = interpolator(image_batch_1, image_batch_2, batch_dt)[0]
-      current_fade_idx = (top_idx+bot_idx)/2
+      current_fade_idx = int((top_idx+bot_idx)/2);
       print(top_idx, target_fade_idx, bot_idx)
-      if current_fade_idx == target_fade_idx:
+      if abs(current_fade_idx - target_fade_idx) <= 1:
         break
       if target_fade_idx < current_fade_idx:
         image_1 = mid_frame
@@ -127,7 +128,8 @@ def _run_interpolator() -> None:
         image_2 = mid_frame
         image_batch_2 = np.expand_dims(image_2, axis=0)
         bot_idx = current_fade_idx
-    
+
+    print("C", current_fade_idx,", T", target_fade_idx)
     mid_frame_filepath = path.join(_FOLDER_OUT.value,"img"+f"{img_idx:05d}"+".png")
     util.write_image(mid_frame_filepath, mid_frame)
     idx+=1
